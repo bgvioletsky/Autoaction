c36385c02f30841e79b4ebc77a156f8272217ba8
bgcode <violetcode@qq.com>
2024-09-06 13:56:27 +0800

    update

---
+++ python/python.py
@@ -0,0 +1,105 @@
+import requests
+import json
+import os
+from datetime import datetime
+# 获取当前时间
+def get_time():
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+    
+# 获取下载链接
+def getsource(name,url, img_url, program_url):
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            releases = response.json()
+            id = releases["id"]
+            assets = releases["assets"]
+            download_urls = []
+            for asset in assets:
+                download_urls.append({"name": asset["name"], "download_url": asset["browser_download_url"]})
+            data={"id": id, "name": name, "program_url": program_url, "img_url": img_url, "download_urls": download_urls}
+            return data
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+
+# 读取json文件
+def read_json(file_path, encoding='utf-8'):
+    try:
+        with open(file_path, 'r', encoding=encoding) as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return None
+    except IOError as e:
+        print(f"An error occurred while reading the file: {e}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+        return None
+
+def save_json(data, encoding='utf-8'):
+    all=[]
+    if data is not None:
+        for item in data:
+            all.append(getsource(item["url"], item["img_url"], item["program_url"], item["name"]))
+    app_path = os.path.join('config', 'all.json')
+    try:
+        with open(app_path, 'w', encoding=encoding) as file:
+            json.dump(all, file, ensure_ascii=False, indent=4)
+        print(f"File '{app_path}' written successfully.")
+    except IOError as e:
+        print(f"An error occurred while writing to the file: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+# 生成消息列表
+def generate_message(data):
+     try:
+            bg=""
+            for url in data["download_urls"]:
+                bg += f"[{url['name']}]({url['download_url']}) \n"
+            name=data["name"]
+            id=data["id"]
+          
+            program_url=data["program_url"]
+            x=f"**{name}**\n**版本 ID: {id}**\n\n[{name} 开源地址]({program_url})\n\n**下载链接**\n\n\n**发布时间: {get_time()}**"
+            return x
+                     
+     except FileNotFoundError:
+        return None
+     except IOError as e:
+        print(f"An error occurred while reading the template file: {e}")
+        return None
+# 发送消息
+def send_message(photo_url,caption):
+    bot_token = os.environ.get('TELEGRAM_TOKEN')
+    chat_id = os.environ.get('TELEGRAM_ID')
+    url = f'https://api.telegram.org/bot{bot_token}/sendPhoto'
+    data = {
+        'chat_id': chat_id,
+        'caption': caption,
+        'parse_mode': 'Markdown'
+    }
+    files = {
+        'photo': requests.get(photo_url).content
+    }
+
+    response = requests.post(url, data=data, files=files)
+    print( response.status_code)
+
+def main():
+    file_path = 'config/download.json'
+    data=read_json(file_path)
+    for item in data:
+        url=item["url"]
+        img_url=item["img_url"]
+        program_url=item["program_url"]
+        name=item["name"]
+        data=getsource(name,url,img_url,program_url)
+        message=generate_message(data)
+        send_message(img_url,message)
+
+
+if __name__ == '__main__':
+    main()
